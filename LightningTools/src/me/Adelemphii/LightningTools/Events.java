ddae2d81d158cada915dffb76ff855d34a59ad7f
Adelemphii <demilitioowner@gmail.com>
2021-01-24 09:50:24 -0600

    Adding files

---
+++ LightningTools/src/me/Adelemphii/LightningTools/Events.java
@@ -0,0 +1,145 @@
+package me.Adelemphii.LightningTools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Events implements Listener {
+	
+	int ATTACK_REACH = 50;
+	
+	public List<String> list = new ArrayList<String>();
+	
+	static Main main;
+	public Events(Main instance) {
+		main = instance;
+	}
+	
+	@EventHandler()
+	public void onClick(PlayerInteractEvent event) {
+		
+		if(event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.NETHERITE_SWORD))
+			if(event.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+				Player player = (Player) event.getPlayer();
+				// Right Click
+				if(event.getAction() == Action.RIGHT_CLICK_AIR) {
+					summonLightning(player);
+				}
+				
+				// Left Click
+				if(event.getAction() == Action.LEFT_CLICK_AIR) {
+					// do somethign with raycasts or something
+					if(!list.contains(player.getName()))
+						list.add(player.getName());
+				}
+			}
+		if(list.contains(event.getPlayer().getName())) {
+			list.remove(event.getPlayer().getName());
+		}
+		
+	}
+	
+	@EventHandler()
+	public void onHit(EntityDamageByEntityEvent event) {
+		if(event.getDamager() instanceof Player) {
+			Player player = (Player) event.getDamager();
+			World world = player.getWorld();
+				// summon lightning
+				Location loc = event.getEntity().getLocation();
+				loc.setY(loc.getY() + 1);
+				
+				for (int i = 1; i < 4; i++) {
+					//loc.getWorld().spawnEntity(loc, EntityType.LIGHTNING);
+					world.strikeLightning(event.getEntity().getLocation());
+					
+				}
+		}
+	}
+	
+	public void summonLightning(Player player) {
+		if(player.getInventory().getItemInMainHand().getType() == Material.NETHERITE_SWORD) {
+			if(player.getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+				
+				Location observerPos = player.getEyeLocation();
+				Vector3D observerDir = new Vector3D(observerPos.getDirection());
+				
+				Vector3D observerStart = new Vector3D(observerPos);
+				Vector3D observerEnd = observerStart.add(observerDir.multiply(ATTACK_REACH));
+				
+				Entity hit = null;
+				
+                // Get nearby entities
+                for (Entity target : player.getWorld().getEntities()) {
+                    // Bounding box of the given player
+                    Vector3D targetPos = new Vector3D(target.getLocation());
+                    Vector3D minimum = targetPos.add(-0.5, 0, -0.5);
+                    Vector3D maximum = targetPos.add(0.5, 1.67, 0.5);
+ 
+                    if (target != player && hasIntersection(observerStart, observerEnd, minimum, maximum)) {
+                        if (hit == null || 
+                                hit.getLocation().distanceSquared(observerPos) > 
+                                target.getLocation().distanceSquared(observerPos)) {
+ 
+                            hit = target;
+                        }
+                    }
+                }
+ 
+                // Hit the closest player
+                if (hit != null) {
+                	if(player.hasLineOfSight(hit)) {
+                	World world = hit.getWorld();
+                	world.strikeLightning(hit.getLocation());
+                	}
+                }
+            }
+        }
+    }
+	
+    // Source:
+   // [url]http://www.gamedev.net/topic/338987-aabb---line-segment-intersection-test/[/url]
+   private boolean hasIntersection(Vector3D p1, Vector3D p2, Vector3D min, Vector3D max) {
+       final double epsilon = 0.0001f;
+
+       Vector3D d = p2.subtract(p1).multiply(0.5);
+       Vector3D e = max.subtract(min).multiply(0.5);
+       Vector3D c = p1.add(d).subtract(min.add(max).multiply(0.5));
+       Vector3D ad = d.abs();
+
+       if (Math.abs(c.x) > e.x + ad.x)
+           return false;
+       if (Math.abs(c.y) > e.y + ad.y)
+           return false;
+       if (Math.abs(c.z) > e.z + ad.z)
+           return false;
+
+       if (Math.abs(d.y * c.z - d.z * c.y) > e.y * ad.z + e.z * ad.y + epsilon)
+           return false;
+       if (Math.abs(d.z * c.x - d.x * c.z) > e.z * ad.x + e.x * ad.z + epsilon)
+           return false;
+       if (Math.abs(d.x * c.y - d.y * c.x) > e.x * ad.y + e.y * ad.x + epsilon)
+           return false;
+
+       return true;
+   }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ LightningTools/src/me/Adelemphii/LightningTools/Main.java
@@ -0,0 +1,77 @@
+package me.Adelemphii.LightningTools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(new Events(this), this);
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(label.equalsIgnoreCase("ls") || label.equalsIgnoreCase("lightningsword")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("Players only!");
+				return true;
+			}
+			
+			Player player = (Player) sender;
+			if(player.getInventory().firstEmpty() == -1) {
+				// inv is full
+				Location loc = player.getLocation();
+				World world = player.getWorld();
+				
+				world.dropItemNaturally(loc, getItem());
+				player.sendMessage(ChatColor.GOLD + "The Minecraft legends have dropped a gift near you!");
+				return true;
+			}
+			player.getInventory().addItem(getItem());
+			player.sendMessage(ChatColor.GOLD + "The Minecraft legends have given you a gift!");
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public ItemStack getItem() {
+		
+		ItemStack sword = new ItemStack(Material.NETHERITE_SWORD);
+		ItemMeta meta = sword.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Lightning Sword");
+		List<String> lore = new ArrayList<String>();
+		lore.add("");
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Right Click) &a&oSummon Lightning"));
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(Left Click) &a&oAttack with lightning or something."));
+		meta.setLore(lore);
+		
+		meta.addEnchant(Enchantment.DAMAGE_ALL, 10, true);
+		meta.addEnchant(Enchantment.SWEEPING_EDGE, 5, true);
+		meta.setUnbreakable(true);
+		
+		sword.setItemMeta(meta);
+		
+		return sword;
+		
+	}
+}
